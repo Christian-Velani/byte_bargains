ad3c382312c4975fdc8b540b654b8f225a916ad8
Christian Velani <chris13082000@gmail.com>
2023-10-23 08:01:39 -0300

    Continuei a trabalhar na tela de jogo

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:blurrycontainer/blurrycontainer.dart';
+import 'package:byte_bargains/styles.dart';
 import 'package:flutter/material.dart';
 
 class JogoPage extends StatefulWidget {
@@ -16,14 +18,53 @@ class _JogoPageState extends State<JogoPage> {
       backgroundColor: Colors.black,
       body: SafeArea(
         top: true,
-        child: FloatingActionButton(
-          onPressed: () => Navigator.pop(context),
-          backgroundColor: Colors.transparent,
-          child: Icon(
-            size: 50,
-            Icons.arrow_circle_left_outlined,
+        child: ListView(children: [
+          Column(
+            children: [
+              Expanded(
+                child: Stack(
+                  clipBehavior: Clip.none,
+                  fit: StackFit.passthrough,
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 300,
+                      child: Image.network(
+                        'https://www.showmetech.com.br/wp-content/uploads//2019/11/Minecraft1-1920x1024.jpg',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    FloatingActionButton(
+                      onPressed: () => Navigator.pop(context),
+                      backgroundColor: Colors.transparent,
+                      child: Icon(
+                        size: 50,
+                        Icons.arrow_circle_left_outlined,
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: BlurryContainer(
+                          width: double.infinity,
+                          child: Column(
+                            children: [
+                              Text(
+                                "Minecraft",
+                                style: textoNotoSansBoldGrande,
+                              ),
+                              Text(
+                                "Minecraft é um jogo eletrônico dos gêneros sandbox e sobrevivência que não possui objetivos específicos a serem alcançados, permitindo aos jogadores uma grande liberdade de escolha de como jogá-lo.",
+                                style: textoOpenSansRegular,
+                              )
+                            ],
+                          )),
+                    )
+                  ],
+                ),
+              ),
+            ],
           ),
-        ),
+        ]),
       ),
     );
   }
